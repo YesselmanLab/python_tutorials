8b1bc8b573ee8e4408186c5c19aae9ebf0838cfa
Ubuntu <ubuntu@ip-172-31-89-96.ec2.internal>
2020-05-19 19:18:50 +0000

    started lesson10.py

---
+++ lesson10.py
@@ -0,0 +1,12 @@
+# Welcome to the tenth lesson in the Yesselman Group's Python series
+# Topics covered: Unittesting and Docstrings
+########################################################################################################################
+# Part I: What is unittesting? Why use it?
+# Now that you can write classes and store them in .py files, you can also start 
+########################################################################################################################
+# Part II: Writing Unittests in Python
+########################################################################################################################
+# Part III: Best practices and using Docstrings
+########################################################################################################################
+# Homework
+
